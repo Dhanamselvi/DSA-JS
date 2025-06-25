da4e9e7ca9ddecced3c1389750feb8cdd40afe33
null <null>
2025-06-25 12:20:57 +0530

    Star_Patterns_Examples

--- src/_3_starPattern.js
+++ src/_3_starPattern.js
@@ -7,65 +7,98 @@ for (let i = 0; i < n; i++) {
   }
   console.log(row);
 }
+/*
+  ****
+  ****
+  ****
+  ****
+*/
 
-for (let i = 0; i < n; i++) {
-  let row = '';
-  for (let j = 0; j <= i; j++) {
-    row += '*';
+// 
+
+for(let i = 0; i < n; i++){
+  let row = "";
+  for(let j = 0; j < i+1; j++){
+    row += "*"
   }
-  console.log(row);
-}
-for (let i = 0; i < n; i++) {
-  let row = '';
-  for (let j = 0; j <= i; j++) {
-    row += j + 1;
-  }
-  console.log(row);
-}
-for (let i = 0; i < n; i++) {
-  let row = '';
-  for (let j = 0; j <= i; j++) {
-    row += i + 1;
-  }
-  console.log(row);
+  console.log(row)
 }
 
-for (let i = 0; i < n; i++) {
-  let row = '';
-  for (let j = 0; j < n - i; j++) {
-    row += j + 1;
+for(let i = 0; i<n; i++){
+  let row = " ";
+  for(let j=0; j<=i; j++){
+    row += ((j+1) + " ") 
   }
-  console.log(row);
-}
-for (let i = 0; i < n; i++) {
-  let row = '';
-  for (let j = 0; j < n - (i + 1); j++) {
-    row += '-';
-  }
-  for (let k = 0; k <= i; k++) {
-    row += '*';
-  }
-  console.log(row);
+  console.log(row)
 }
 
-for (let i = 0; i < n; i++) {
-  let row = '';
-  let flip = 1;
-  for (let j = 0; j <= i; j++) {
-    row += flip;
-    if (flip == 0) flip = 1;
-    else flip = 0;
+/* For Above both loop the same ouptput
+  *
+  **
+  ***
+  ***
+*/
+
+for(let i = 0; i < n; i++){
+  let row = "";
+  for(let j = 0; j <=i; j++){
+    row += ((i + 1) + " ")
   }
-  console.log(row);
+  console.log(row)
 }
+/*
+  1
+  2 2
+  3 3 3
+  4 4 4 4
+*/
+
+for(let i = n; i >= 0; i--){
+  let row = "";
+  for(let j = 0; j <= i; j++){
+    row += j+1
+  }
+  console.log(row)
+}
+
+/*
+  1 2 3 4
+  1 2 3
+  1 2
+  1
+*/
+
+for(let i = 0; i < n; i++){
+  let row = "";
+  for(let j = 0; j < (n - (i+1)); j++){
+    row += " "
+  }
+  for(let k = 0; k < (i+1); k++){
+    row += "*"
+  }
+  console.log(row)
+}
+
+/*
+        *
+      * *
+    * * *
+  * * * *
+*/
 
 let flip = 1;
-for (let i = 0; i < n; i++) {
-  let row = '';
-  for (let j = 0; j <= i; j++) {
+for(let i = 0; i<n; i++){
+  let row = "";
+  for(let j = 0; j<=i; j++){
     row += flip;
-    if (flip == 0) flip = 1;
-    else flip = 0;
+    if(flip == 1) flip = 0;
+    else flip = 1
   }
-  console.log(row);
+  console.log(row)
 }
+/*
+  1
+  1 0
+  1 0 1
+  1 0 1 0
+*/
