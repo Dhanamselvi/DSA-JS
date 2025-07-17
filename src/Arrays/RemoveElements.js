56cddba2c2f81ba88eab2d1b90f08c7c85b060fe
Dhanamselvi <selviace92@gmail.com>
2025-07-17 09:48:45 +0530

    RemoveElementsFromArray

---
+++ src/Arrays/RemoveElements.js
@@ -0,0 +1,18 @@
+// Remove Elements
+/*
+    Given an interger array nums and an integer val,
+    remove all the accuances val in nums in-place.
+    The order of the elements may be changed.
+    Then return the number of elements in nums which are not equal to val.
+*/
+
+function removeElements(arr,val){
+    let x = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] != val){
+            x = x + 1;
+        }
+    }
+    return x;
+}
+console.log(removeElements([1,2,3,2,4,2,5,2],2))
