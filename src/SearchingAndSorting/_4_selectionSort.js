97db3d9870afd122786d27474f02451d29270202
Dhanamselvi <selviace92@gmail.com>
2025-08-19 17:54:44 +0530

    selectionSorting

---
+++ src/SearchingAndSorting/_4_selectionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(arr){
+    let n = arr.length;
+    for(let i = 0; i < n-1; i++){
+        let min = i;
+        for(let j = i + 1; j < n; j++){
+            if(arr[j] < arr[min]){
+               min = j
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr
+}
+
+console.log(selectionSort([7,1,3,4,5,2]))
